6c1aeb65a8bc525382dd59442f067bc6a4ac4041
Chrissy Wenzel <Christina_Regina@hotmail.de>
2023-07-02 10:13:22 +0200

    Test 1

---
+++ driveTest.py
@@ -0,0 +1,25 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from datetime import datetime, timedelta
+
+gauth = GoogleAuth(settings_file='C:\\Users\\Chris\\Documents\\Semester_8\\Bundesgartenschau\\settings.yaml')
+#gauth.DEFAULT_SETTINGS['client_config_file'] = 'C:\\Users\\Chris\\Documents\\Semester_8\\Bundesgartenschau\\Credentials.json'
+drive = GoogleDrive(gauth)
+
+#gfile = drive.CreateFile({'title': 'allgemeine-informationen.txt','parents': [{'id': '1NggFkhUZ1LmAEObTvGU7H9sfLc9QKW-B'}]})
+#gfile.SetContentFile('C:\\Users\\Chris\\Documents\\Semester_8\\Bundesgartenschau\\allgemeine-informationen.txt')
+#gfile.Upload()
+
+#file = drive.CreateFile({'id': '1H2gFWfNR_tKqlj7HGooZQZy9iZOC-QLt'})
+#content_string = file.getContentString()
+
+#print(content_string)
+#file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format('1NggFkhUZ1LmAEObTvGU7H9sfLc9QKW-B')}).GetList()
+#for file in file_list:
+#	print('title: %s, id: %s' % (file['title'], file['id']))
+
+file7 = drive.CreateFile({'id': '1MUtalibYpvJ1FBHQGzDC4wEW19xL0wZw'})
+content = file7.GetContentString()
+print(content)
+#file = drive.CreateFile({'id': '1NggFkhUZ1LmAEObTvGU7H9sfLc9QKW-B'})
+#file.GetContentFile('test.txt')
